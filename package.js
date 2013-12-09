22a618b53408f9fb43a6e39585e4c9c8755ea8a5
Lane <lolson@ualberta.ca>
2013-12-09 16:05:48 -0700

    updating packages

--- package.js
+++ package.js
@@ -6,27 +6,20 @@ Package.describe({
  * Ex: Some NPM Dependencies
  */
 Npm.depends({
-  'querystring': {},
-  'crypto': {},
-  'fs': {},
-  'https': {},
-  'url': {},
-  'request': {}
+  'querystring': '0.2.0',
+  'crypto': '0.0.3',
+  'url': '0.7.9',
+  'request': '2.29.0'
 });
 
 /**
  * On use we'll add files and export our tool
  */
 Package.on_use(function (api) {
-  /**
-   * Add all the files, in the order of their dependence (eg, if A.js depends on B.js, B.js must be before A.js)
-   */
+
   api.add_files(['lib/lti-auth.js'],'server'); // Can be 'server', 'client' , ['client','server']
 
-  /**
-   * Only expose the My constructor, only export if meteor > 6.5
-   */
   if (typeof api.export !== 'undefined') {
-    api.export(['Provider'], 'server'); // 1st arg can be array of exported constructors/objects, 2nd can be 'server', 'client', ['client', 'server']
+    api.export(['Provider'], 'server');
   }
 });
